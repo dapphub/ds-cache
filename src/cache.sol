6b6e3d4eed1d790c7b1d9db8a50d985cb7c2f767
Mariano Conti <nanexcool@gmail.com>
2017-08-30 15:45:28 -0300

    pragma 0.4.13

--- src/cache.sol
+++ src/cache.sol
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pragma solidity ^0.4.8;
+pragma solidity ^0.4.13;
 
 import 'ds-value/value.sol';
 
